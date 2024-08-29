4764d9edc84f91bad04753d5fc5757111014a4d7
ValeriaPostylyakova <postylyakova.valeria4020@mail.ru>
2024-08-29 19:08:19 +0300

    получение корзиныÐ с mokky dev

--- src/components/DrawerItems.jsx
+++ src/components/DrawerItems.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { clearDrawer } from '../redux/slices/drawerSlice.js';
